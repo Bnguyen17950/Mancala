c7e3b0f319d9fe1881d1fd523e7bf84871a79b6c
kyan215 <kevinyan215@gmail.com>
2015-04-23 19:48:50 -0700

    4.23.15
    changes on view

--- src/View.java
+++ src/View.java
@@ -1,4 +1,5 @@
 import java.awt.Container;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -15,13 +16,10 @@ public class View extends JFrame
 {
         //update information (Contains all the listeners)
         // Contains buttons
-        final private JFrame frame;
+        private JFrame frame;
         final Container pane;
         private DefaultTableModel TableModel;
         final JPanel pnlMancala;
-        final JLabel PitLabelsA;
-        final JLabel PitLabelsB;
-        final Model newModel;
         private JButton mancalaA;
         private JButton mancalaB;
         private JLabel mancalaPlayerA;
@@ -32,7 +30,8 @@ public class View extends JFrame
         private ArrayList<JButton> mancala = new ArrayList<>();
         private ArrayList<JLabel> playerLabel = new ArrayList<>();
         private ArrayList<JLabel> pitLabel = new ArrayList<>();
-        
+        private ArrayList<JButton> allButtons = new ArrayList<>();
+         
         public View() {
             //prepare frame
             frame = new JFrame("Mancala Board");
@@ -41,7 +40,7 @@ public class View extends JFrame
             pane.setLayout(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             pnlMancala = new JPanel(null);
-            newModel = new Model(0); // added a zero because of error of int
+            
             
             //creates mancala B and mancala A
             int x = 80;
@@ -50,6 +49,7 @@ public class View extends JFrame
             	mancala.get(i).setBounds(x,100,60,300);
             	x += 780;
             	pnlMancala.add(mancala.get(i));
+            	
             }
             
             //creates the label for the mancalas
@@ -68,30 +68,57 @@ public class View extends JFrame
             //creates row of A pits
             x = 200;
             for(int i = 0; i < 6; i++){
-            	AJButtons.add(new JButton("A"));
+            	AJButtons.add(new JButton());
             	AJButtons.get(i).setBounds(x, 260, 100, 40);
             	x += 100;
             	pnlMancala.add(AJButtons.get(i));
+            	String buttonNumber = "A" + (i+1);
+            	AJButtons.get(i).setName(buttonNumber);
+//            	AJButtons.get(i).setText(buttonNumber);
             }
          
            
             //creates row of B pits
-             x = 200;
+             x = 700;
             for(int i = 0; i < 6; i++){
-            	BJButtons.add(new JButton("B"));
+            	BJButtons.add(new JButton());
             	BJButtons.get(i).setBounds(x, 220, 100, 40);
-            	x += 100;
+            	x -= 100;
+            	String buttonNumber = "B" + (i+1);
+            	BJButtons.get(i).setName(buttonNumber);
+//            	BJButtons.get(i).setText(buttonNumber);
             	pnlMancala.add(BJButtons.get(i));
             }
             
-            //creates labels for pits
-            String space = "                            "; 
-            PitLabelsB = new JLabel("B6" + space + "B5" + space + "B4" + space + "B3" + space + "B2" + space + "B1"); //4.17.15
-            PitLabelsA = new JLabel("A1" + space + "A2" + space + "A3" + space + "A4" + space + "A5" + space + "A6"); //4.17.15
-            pitLabel.add(PitLabelsB);
-            pitLabel.add(PitLabelsA); 
-            PitLabelsA.setBounds(250, 140, 600, 350);
-            PitLabelsB.setBounds(250, 80, 600, 250);
+            //inserts all the buttons into one arraylist
+            for(int i = 0; i < AJButtons.size(); i++){
+            	allButtons.add(AJButtons.get(i));
+            }
+            for(int i = 0; i < BJButtons.size(); i++){
+            	allButtons.add(BJButtons.get(i));
+            }
+            
+         // Creates labels for Player A's pits
+            for(int i = 0; i < 6; i++)
+            {
+             final JLabel aLabel = new JLabel("A" + (i+1));
+             pitLabel.add(aLabel);
+             
+             int xCoord = AJButtons.get(i).getX() + AJButtons.get(i).getWidth() / 2;
+             int yCoord = AJButtons.get(i).getY() + AJButtons.get(i).getHeight();
+             aLabel.setBounds(xCoord, yCoord, AJButtons.get(i).getWidth(), AJButtons.get(i).getHeight()/2);
+            }
+            
+            // Creates labels for Player B's pits
+            for(int i = 0; i < 6; i++)
+            {
+             final JLabel bLabel = new JLabel("B" + (6 - i));
+             pitLabel.add(bLabel);
+             
+             int xCoord = BJButtons.get(i).getX() + BJButtons.get(i).getWidth() / 2;
+             int yCoord = BJButtons.get(i).getY() - BJButtons.get(i).getHeight() /2 ;
+             bLabel.setBounds(xCoord, yCoord, BJButtons.get(i).getWidth(), BJButtons.get(i).getHeight()/2);
+            }
             for(JLabel label: pitLabel){
             	pnlMancala.add(label);
             }
@@ -103,8 +130,37 @@ public class View extends JFrame
             frame.setResizable(true);
             frame.setVisible(true);
         }
-
-        public void showPlayersTurn() {
-            //newPlayer
+        public ArrayList<JButton> getPits(){
+        	return allButtons;
         }
+        
+        public void addPitActionListener(ActionListener l){
+        	for(JButton button: allButtons){
+        		button.addActionListener(l);
+        	}
+        }
+        //controller function. implementation of the pit's actionlistener
+//        public void setPits(){
+//        	//pits
+//        	ArrayList<JButton> allPits = view.getPitArrayList();
+//        	array modelArray = model.getArray();
+//        	for(int i =0; i < allPits.size(); i++){
+//        		String stones = setStones(modelArray[i]);
+//        		allPits.get(i).setText(stones);
+//        	}
+//        	
+//        	//order of each player has to match between the two arraylist of Jbutton and players
+//        	//same thing for player's mancala. 
+//        	ArrayList<Player> players = new ArrayList<Player>();
+//        	players.add(model.getPlayerA());
+//        	players.add(model.getPlayerB(); 
+//        	ArrayList<JButton> mancalaButton = view.getMancalaButtons();
+//  
+//        	for(int i = 0; i < players.size(); i++){
+//        		String stones = setStones(players.getStonesInMancala());
+//        		mancalaButton.get(i).setText(stones);
+//        	}
+//        }
+        
+   
     }
