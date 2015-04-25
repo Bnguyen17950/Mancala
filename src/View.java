70e1de7bf8a7dc98d9b7fc8a6a9151e9e06c1f19
kyan215 <kevinyan215@gmail.com>
2015-04-24 23:19:58 -0700

    testing. unimportant change

--- src/View.java
+++ src/View.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 
 /**
- * update kevin
+ * update kevin 11:18
  * Created by bryannguyen on 4/3/15.
  */
 public class View extends AbstractStrategy
